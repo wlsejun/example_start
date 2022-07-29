6a89fbe200d696433fcb5cd3d671e42708267425
yesejun-can <worldlight97@daum.net>
2022-07-29 15:46:26 +0900

    study: form

---
+++ nodejs/220729/file-upload실습.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+const upload = multer({
+    storage: multer.diskStorage({
+        destination(req, file, done){
+            done(null, 'uploads/');
+        },
+        filename(req, file, done) {
+            const ext = path.extname(file.originalname);
+            done(null, path.basename(file.originalname, ext) + Data.now() + ext );
+        },
+    }),
+    limits: {fileSize : 5*1024*1024 }
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", test, test2, function(req,res) {
+    res.render("index");
+});
+
+app.post("/upload/array", upload.array('userfile'), function(req,res) {
+    console.log(req.body);
+    console.log(req.files);
+    res.send("Upload Array");
+    
+});
+
+function test(req, res, next) {
+    console.log( "test함수 ");
+    console.log( req.path );
+    next();
+}
+function test2(req, res, next ) {
+    console.log( "Test2 함수");
+    next();
+}
+app.listen(port, ()=>{
+    console.log("Server Port : ", port);
+})
