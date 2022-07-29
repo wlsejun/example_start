a0a087e017a82d4f17d45016238663c9b1171051
yesejun-can <worldlight97@daum.net>
2022-07-29 18:41:41 +0900

    study: file-upload 실습

--- nodejs/220729/file-upload실습.js
+++ nodejs/220729/file-upload실습.js
@@ -4,32 +4,30 @@ const port = 8080;
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
+
 const upload = multer({
     storage: multer.diskStorage({
         destination(req, file, done){
+            console.log( "destination : ", req.body );
             done(null, 'uploads/');
         },
         filename(req, file, done) {
             const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Data.now() + ext );
+            done(null, req.body.id + ext );
         },
     }),
     limits: {fileSize : 5*1024*1024 }
 });
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.get("/", test, test2, function(req,res) {
+app.get("/", function(req,res) {
     res.render("index");
 });
+app.get("/register", function(req,res) {
+    res.render("register");
+});
 
-app.post("/upload/array", upload.array('userfile'), function(req,res) {
-    console.log(req.body);
-    console.log(req.files);
-    res.send("Upload Array");
+app.post("/register", upload.single('userfile'), function(req,res) {
+    res.render("result", { filename : req.file.filename });
     
 });
 
