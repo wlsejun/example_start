c5408162819b1189080d4fc974c03e11da579e4c
yesejun-can <worldlight97@daum.net>
2022-08-12 16:33:42 +0900

    study: session

--- nodejs/220812/index.js
+++ nodejs/220812/index.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(cookieParser('1234'));
+app.use(session({
+    secret : 'secret key',
+    // resave : false,
+    // saveUninitialized : true
+}))
 
 const cookieConfig = {
     maxAge: 30000,
-    path: '/',
+    path: '/', //시작경로 ex) /get : get으로 시작하는 모든 쿠키를 볼 때
     httpOnly: true,
     signed: true, //암호화
 };
 
-app.get("/", (req,res) => {
+app.get("/index", (req,res) => {
+    req.session.name = "홍길동";
     res.cookie('key', 'value', cookieConfig);
     res.cookie('key2', 'value2', cookieConfig);
     res.render("index");
 });
 
 app.get("/get", (req,res)=> {
+    console.log( req.session.name );
     console.log( req.cookies );
     res.send( req.cookies );
 })
 app.get("/cookie", (req,res) => {
     res.render("cookie");
+})
+app.get("/destroy", (req,res) => {
+    req.session.destroy(function(err){
+        res.send("삭제");
+    })
+    req.session.name = "";
 })
 
 app.listen(8000, ()=>{
